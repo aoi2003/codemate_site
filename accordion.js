2a0e20478fb6f0f8ff12bed39c00a136c8ccae9a
A01 <koukou20030304@yahoo.co.jp>
2024-08-25 20:09:21 +0900

    アコーディオンの変更

--- accordion.js
+++ accordion.js
@@ -1,22 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const accordionButtons = document.querySelectorAll('.accordion-button');
-  
-  accordionButtons.forEach(button => {
-      button.addEventListener('click', function() {
-          const content = this.nextElementSibling;
-          const icon = this.querySelector('.accordion-icon');
-          const isExpanded = this.getAttribute('aria-expanded') === 'true';
-          
-          this.setAttribute('aria-expanded', !isExpanded);
-          content.classList.toggle('active');
-          
-          if (isExpanded) {
-              icon.textContent = '＋';
-          } else {
-              icon.textContent = 'ー';
-          }
-      });
-  });
+    const accordionButtons = document.querySelectorAll('.accordion-button');
+    
+    accordionButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const content = this.nextElementSibling;
+            const isExpanded = this.getAttribute('aria-expanded') === 'true';
+            
+            this.setAttribute('aria-expanded', !isExpanded);
+            content.classList.toggle('active');
+        });
+    });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
